2946985386dfccb0a1e766f11788b5a6e0c66959
n.but <but.nikolay@gmail.com>
2018-06-20 16:49:18 +0300

    comments move variables from global to local

--- app/js/comments.js
+++ app/js/comments.js
@@ -81,40 +81,6 @@ const comments = {
     xhr.send();
   },
 
-
-  add(elem) {
-    let userNameInput = $(elem).siblings(`#nameUser`);
-    let userMessageInput = $(elem).siblings(`#commentUser`);
-    let userName = userNameInput.val();
-    let userMessage = userMessageInput.val();
-    if (userName && userMessage) {
-      let userId = Math.floor(Math.random() * 10 * userName.length);
-      this.ajax({
-        url: this.settings.urlAddComment,
-        data: {
-          id_user: userId,
-          text: userMessage
-        },
-        success: data => {
-          (function (data, elem, arrComments) {
-            if (data.result && data.result === 1) {
-              const newComment = new Comment(userId, userMessage);
-              newComment.render(elem);
-              arrComments.push({
-                "id_comment": userId,
-                "text": userMessage
-              });
-              userNameInput.val('');
-              userMessageInput.val('');
-            } else if (data.result === 0) {
-              alert(`Сервер вернул ошибку: ${data.error_message}`);
-            }
-          }(data, this.$elCommentBlock, this.arrAllComments))
-        }
-      });
-    }
-  },
-
   makeForm() {
     //создаем обертку для модуля комментариев
     const containerForm = document.createElement('form');
@@ -126,13 +92,35 @@ const comments = {
     //запомним созданный элемент в переменной
     this.formEl = containerForm;
 
-    //навешиваем обработчик события отправки формы
-    containerForm.addEventListener('submit', (event) => {
+    //создаем функцию отправки нового комментария
+    const add = (event) => {
+
+      const showErrorMessage = (textArea) => {
+        //проверяем создана и висит ли уже ошибка
+        if (textArea.parentElement.querySelector(`.${this.settingsComments.classErrorText}`)) return;
+        //добавляем стиль ошибки на textarea
+        textArea.classList.add(this.settingsComments.classErrorTextarea);
+        //создаем span
+        const hint = document.createElement('span');
+        //вкладываем в него текст
+        hint.textContent = 'Text must contain at least 1 character';
+        //добавляем класс
+        hint.classList.add(this.settingsComments.classErrorText)
+        //вставляем элемент на страницу
+        textArea.parentElement.append(hint);
+      };
+      
+      const isError = (textArea, userRegexp) => {
+        //создаем регулярку из переданного в настройках
+        const regexp = new RegExp(`${userRegexp}`, `gi`);
+        //возвращаем результат проверки - если все ок, то false, если проверка не прошла - true
+        return !regexp.test(textArea.value);
+      };
       event.preventDefault();
-      console.log(this);
-
-      if (this.isError(this.textAreaEl, this.settingsComments.commentRegexp)) return this.showErrorMessage(this.textAreaEl);
-
+      //проверим на наличие ошибок textarea (пока просто должен быть хотя бы 1 символ)
+      if (isError(this.textAreaEl, this.settingsComments.commentRegexp)) {
+        return showErrorMessage(this.textAreaEl);
+      }
       let that = this;
       let xhr = new XMLHttpRequest();
       xhr.open('GET', that.settingsComments.urlAddComment, true);
@@ -140,16 +128,18 @@ const comments = {
         if (this.status === 200) {
           console.log(JSON.parse(this.response));
         }
-      }
+      };
       xhr.send();
-
       //как заглушка просто добавляем в массив с комментами новый коммент
       this.comments.push({
         id_comment: this.comments.length + 1,
         text: this.textAreaEl.value,
       });
       this.render();
-    })
+    };
+
+    //навешиваем обработчик события отправки формы
+    containerForm.addEventListener('submit', add)
 
     //создаем заголовок h3
     const formTitle = document.createElement('h3');
@@ -167,14 +157,26 @@ const comments = {
     inputTextArea.classList.add(this.settingsComments.classTextarea);
     this.textAreaEl = inputTextArea;
 
+    const textareaHandleHeight = (inputTextArea, rowsDefault) => {
+      // Установим дефолтную высоту textarea
+      inputTextArea.rows = rowsDefault;
+      //создадим метод, который 
+      const stretchHeight = function () {
+        let counter = this.value.split("\n").length;
+        if (counter <= rowsDefault) return;
+        else if (counter > rowsDefault) this.rows = counter;
+      };
+      //повесим обработчик на отслеживание првышения высоты по умолчанию и если да то увеличиваем rows у textarea
+      inputTextArea.addEventListener('input', stretchHeight);
+    };
+    
     //применяем метод которые увеличивает высоту и задает высоту по умолчанию
-    this.textareaHandleHeight(inputTextArea, 5);
+    textareaHandleHeight(inputTextArea, 5);
 
     //создаем кнопку отправки комментария
     const submitBtn = document.createElement('button');
     submitBtn.classList.add(this.settingsComments.classFormSubmitBtn);
     submitBtn.textContent = 'Submit comment';
-
 
     //вставляем все элементы в обертку
     containerForm.append(formTitle);
@@ -184,41 +186,8 @@ const comments = {
     //добавляем обертку на страницу
     this.commentEl.append(containerForm);
   },
+
   
-  isError(textArea, userRegexp) {
-    //создаем регулярку из переданного в настройках
-    const regexp = new RegExp(`${userRegexp}`, `gi`);
-    //возвращаем результат проверки - если все ок, то false, если проверка не прошла - true
-    return !regexp.test(textArea.value);
-  },
-  
-  showErrorMessage(textArea) {
-    //проверяем создана и висит ли уже ошибка
-    if (textArea.parentElement.querySelector(`.${this.settingsComments.classErrorText}`)) return;
-    //добавляем стиль ошибки на textarea
-    textArea.classList.add(this.settingsComments.classErrorTextarea);
-    //создаем span
-    const hint = document.createElement('span'); 
-    //вкладываем в него текст
-    hint.textContent = 'Text must contain at least 1 character';
-    //добавляем класс
-    hint.classList.add(this.settingsComments.classErrorText)
-    //вставляем элемент на страницу
-    textArea.parentElement.append(hint);
-  },
-  
-  textareaHandleHeight(inputTextArea, rowsDefault) {
-    // Установим дефолтную высоту textarea
-    inputTextArea.rows = rowsDefault;
-    //создадим метод, который 
-    const stretchHeight = function () {
-      let counter = this.value.split("\n").length;
-      if (counter <= rowsDefault) return;
-      else if (counter > rowsDefault) this.rows = counter;
-    };
-    //повесим обработчик на отслеживание првышения высоты по умолчанию и если да то увеличиваем rows у textarea
-    inputTextArea.addEventListener('input', stretchHeight);
-  },
 
   render() {
     //предварительно очистим внутренности - нужно для перерисовки
